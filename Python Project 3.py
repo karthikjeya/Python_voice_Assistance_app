73bf96a300a2259a6720c80e876bae682c5b3482
Karthik <94369594+karthikjeya@users.noreply.github.com>
2025-08-11 15:25:21 +0530

    Add files via upload

---
+++ Python Project 3.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr
+
+while True:
+
+                      r = sr.Recognizer()
+                      with sr.Microphone() as source:
+                          audio = r.listen(source)
+
+                      try:
+                         filename = "draft.txt"
+                         f = open(filename, "a+")
+
+                         recognized_text = r.recognize_google(audio)
+                         print(recognized_text)
+                         remainder = recognized_text.split()
+                         while remainder:
+                             line, remainder = remainder[:5], remainder[5:]
+                             f.write(' '.join(line) + "\n")
+                         if recognized_text == 'exit':
+                             break
+                      except sr.UnknownValueError:
+                         print("Google could not understand audio")
+                      except sr.RequestError as e:
+                         print("Google error; {0}".format(e))
+                      finally:
+                          print("successfull")
+
+                      
+    
+        
